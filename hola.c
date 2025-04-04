b1402ac13eeb31b527654387aeb71982f7a6a675
s <Estudiante@pc27.lab1105.labos.fcen.uba.ar>
2025-04-03 21:56:17 -0300

    ej3 terminado

--- hola.c
+++ hola.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola Orga!\n");
-    return 0;
+    char c = 100;
+    short s = -34;
+    int i = 20345;
+    long l = 20234893435L;
+
+    printf("Tamaño: %lu, valor: %d \n", sizeof(c), c);
+    printf("Tamaño: %lu, valor: %d \n", sizeof(s), s);
+    printf("Tamaño: %lu, valor: %d \n", sizeof(i), i);
+    printf("Tamaño: %lu, valor: %ld \n", sizeof(l), l);
 }
