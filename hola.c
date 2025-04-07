e65916b0c4cd0d3783dc105743267f6906c9b0f0
simonguerenu <simonguerenu@gmail.com>
2025-04-07 02:49:38 -0300

    Completo ej12

--- hola.c
+++ hola.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <inttypes.h>
 
 void ej3() {
@@ -76,10 +77,103 @@ void ej7(){
     int c = 2;
     int d = 1;
     int resultado1 = a + b * c / d;
-    printf("%d", resultado1); 
+    int resultado2 = a % b;
+    int resultado3_1 = a == b;
+    int resultado3_2 = a != b;
+    int resultado4_1 = a & b;
+    int resultado4_2 = a | b;
+    int resultado5 = ~a;
+    int resultado6_1 = a && b;
+    int resultado6_2 = a && b;
+    int resultado7 = a << 1;
+    int resultado8 = a >> 1;
+    int resultado9_1 = a+=b;
+    int resultado9_2 = a-=b;
+    int resultado9_3 = a*=b;
+    int resultado9_4 = a/=b;
+    int resultado9_5 = a%=b;
+    printf("Resultado 1: %d\n", resultado1);
+    printf("Resultado 2: %d\n", resultado2);
+    printf("Resultado 3: %d y %d\n", resultado3_1, resultado3_2);
+    printf("Resultado 4: 0x%x y 0x%X\n", resultado4_1, resultado4_2);
+    printf("Resultado 5: 0x%x\n", resultado5);
+    printf("Resultado 7: %x\n", resultado7);
+    printf("Resultado 9_1: %d\n", resultado9_1);
+    printf("Resultado 9_2: %d\n", resultado9_2);
+    printf("Resultado 9_3: %d\n", resultado9_3);
+    printf("Resultado 9_4: %d\n", resultado9_4);
+    printf("Resultado 9_5: %d\n", resultado9_5); 
+}
+
+void ej8(){
+    int a = 4;
+    int a_antes = ++a;
+    printf("Valor ++a: %d\n", a_antes);
+    printf("Valor a despues: %d\n", a);
+    int b = 4;
+    int b_despues = b++;
+    printf("\nValor b++: %d\n", b_despues);
+    printf("Valor b despues: %d\n", a);
+} 
+
+void ej9(){
+    unsigned int a = 0xA0C91919; //2697533721
+    unsigned int b = 0x84C9BF5A; //2227814234
+    a = a >> 29;
+    b = b << 29;
+    b = b >> 29;
+    int resultado1 = a == b;
+
+    unsigned int c = 0xA0C91914; 
+    unsigned int d = 0x85EF9995; 
+    c = c >> 29;
+    d = d << 29;
+    d = d >> 29;
+    int resultado2 = c==d;
+    printf("Resultado 1: %d\n", resultado1);
+    printf("Resultado 2: %d", resultado2);
+    
+}
+
+void ej10(){
+    int i = 0;
+    while(i < 10){
+        printf("%d\n", i);
+        i++;
+    }
+}
+
+void ej11(){
+    int arreglo[] = {1, 2, 3, 4};
+    int primeraPos = arreglo[0]; 
+    for(int i = 0; i < 3; i++){
+        arreglo[i] = arreglo[i+1];
+    }
+    arreglo[3] = primeraPos;
+    for(int i = 0; i < 4; i++){
+        printf("%d,", arreglo[i]);
+    }
+}
+
+void ej12(int rotacion){
+    int rotacionMod = rotacion % 4;
+    int arreglo[] = {1, 2, 3, 4};
+    int arregloCopia[] = {1, 2, 3, 4};  
+    for(int i = 0; i < 4; i++){
+        arreglo[(i-rotacionMod + 4) % 4] = arregloCopia[i];
+    }
+    for(int i = 0; i < 4; i++){
+        printf("%d,", arreglo[i]);
+    }
+    printf("\n");
 }
 
 int main(){
-    ej7();
+    ej12(0);
+    ej12(1);
+    ej12(2);
+    ej12(3);
+    ej12(4);
+    ej12(5);
 }
 
