9285d32abc681628668e4692e1d7afdbe3906772
simonguerenu <simonguerenu@gmail.com>
2025-04-26 21:20:58 -0300

    proceseso4

--- asm/memoria_din/Memoria.asm
+++ asm/memoria_din/Memoria.asm
@@ -15,7 +15,33 @@ global strLen
 ; ** String **
 
 ; int32_t strCmp(char* a, char* b)
+;a-->rdi, b-->rsi
 strCmp:
+	xor rax, rax
+	mov al, BYTE [rdi]
+	mov cl, BYTE [rsi]
+	loop_chequeo_letra:
+		cmp al, cl
+		jne char_differ
+		mov r8b, al
+		or r8b, cl
+		cmp r8b, 0
+		je equals
+
+		add rdi, 1
+		add rsi, 1
+		mov al, BYTE [rdi]
+		mov cl, BYTE [rsi]
+		jmp loop_chequeo_letra
+
+	equals:
+		mov rax, 0
+		jmp fin
+	char_differ:
+		sub al, cl
+		sar al, 31
+		or al, 1
+	fin:
 	ret
 
 ; char* strClone(char* a)
